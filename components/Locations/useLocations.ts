63adf0aa87274567bdfc649848733e2cfeb9f4c9
Greg Cumming <KiwiKid@users.noreply.github.com>
2021-12-29 02:23:32 +1300

    Added more location overrides (for missing lat/lngs)

--- components/Locations/useLocations.ts
+++ components/Locations/useLocations.ts
@@ -11,12 +11,22 @@ type LocationOverride = {
 
 let LOCATION_OVERRIDES:LocationOverride[] = [
   {eventId: 'a0l4a0000006NKfAAM', lat: '-39.550910', lng: '174.123688'}
+  ,{eventId: 'a0l4a0000006RWCAA2', lat: '-38.689185', lng: '176.071376'}
+  , {eventId: 'a0l4a0000006QRRAA2', lat: '-37.673978143585785', lng: '176.22322409540428'}
+  , {eventId: 'a0l4a0000006NXZAA2', lat: '-37.710854952237725', lng: '176.15152478126316'}
+  , {eventId: 'a0l4a0000006OyMAAU', lat: '-37.67357686829962', lng: '176.22265982164015'}
+  , {eventId: 'a0l4a0000006Q1yAAE', lat: '-38.14035277924268', lng: '176.25445811133048'}
+  , {eventId: 'a0l4a0000006Sp4AAE', lat:'-37.73870099757525', lng: '176.1038195083869'}
+  , {eventId: 'a0l4a0000006SopAAE', lat: '-38.68647464948843', lng: '176.06532281197366'}
+  , {eventId: 'a0l4a0000006SXuAAM', lat: '-35.93501809911511', lng: '173.87742493344686'}
+  , {eventId: 'a0l4a0000006QSyAAM', lat: '-37.70737160447246', lng: '176.14647672692442'}
+  , {eventId: 'a0l4a0000006QTDAA2', lat: '-37.70451889546021', lng:'176.15319463761546'}
+  , {eventId: 'a0l4a0000006QPVAA2', lat: '-37.682077131055124', lng: '176.16832800001407'}
+  , {eventId: 'a0l4a0000006QP1AAM', lat: '-37.674881587012045', lng:'176.22266988519743'}
 ]
 
 export default function useLocations() {
     const { data, error } = useSWR<LocationAPIResponse>(`/api/locations/`, {fetcher: fetcher, refreshInterval: 60000*30} )
-
-
 
     const formattedLocations = data?.locations
         .map(applyLocationOverrides)
