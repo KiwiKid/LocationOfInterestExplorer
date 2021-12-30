0c236134028a227f1b5f8ee51248f2a681119c4d
Greg Cumming <KiwiKid@users.noreply.github.com>
2021-12-31 09:39:46 +1300

    Remove manual refresh interval to address cached data
    Old data being displayed - requiring a refresh to see new locations

--- components/Locations/useLocations.ts
+++ components/Locations/useLocations.ts
@@ -26,7 +26,7 @@ let LOCATION_OVERRIDES:LocationOverride[] = [
 ]
 
 export default function useLocations() {
-    const { data, error } = useSWR<LocationAPIResponse>(`/api/locations/`, {fetcher: fetcher, refreshInterval: 60000*30} )
+    const { data, error } = useSWR<LocationAPIResponse>(`/api/locations/`, {fetcher: fetcher } )
 
     const formattedLocations = data?.locations
         .map(applyLocationOverrides)
