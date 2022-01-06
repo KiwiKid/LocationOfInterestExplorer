9534a384bdc58f88dc3ed95aa8b4a20be58448ef
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-01-07 00:50:54 +1300

    Wait for page to load before screenshotting

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -134,6 +134,7 @@ const handler = async (req:NextApiRequest, res:NextApiResponse) => {
             </style>
         </html>`);*/
         page.goto(`https://nzcovidmap.org/${reqQuery}`);
+        page.waitForText("Locations of interest since");
         const screenShotBuffer = await page.screenshot();
         if(!!screenShotBuffer){
             res.writeHead(200, {
