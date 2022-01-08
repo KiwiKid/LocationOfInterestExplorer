89e80af52512200012fe95666be302ab3da8b87b
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-01-09 09:52:43 +1300

    Fix preview link

--- pages/api/Image/loc/[quickLink].ts
+++ pages/api/Image/loc/[quickLink].ts
@@ -142,7 +142,7 @@ const handler = async (req:NextApiRequest, res:NextApiResponse) => {
         
         
        
-        let url = `${getHardCodedUrl()}/?${quickLink}`;
+        let url = `${getHardCodedUrl()}/loc/${encodeURIComponent(quickLink)}`;
         console.log(`Going to page ${url}`);
         page.goto(url);
         console.log(`Waiting for .leaflet-container`);
