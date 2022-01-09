f8e046088b7c415394a7388ba5fed7c53b75997c
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-01-09 13:09:00 +1300

    Improve today summary

--- components/Locations/TodayLocationSummary.tsx
+++ components/Locations/TodayLocationSummary.tsx
@@ -1,11 +1,22 @@
 import { LocationOfInterest } from "../types/LocationOfInterest"
 import CopyBox from "../utils/CopyBox"
+import { getHardCodedUrl } from "../utils/utils"
 import { startOfDay } from "./DateHandling"
 import { processGroupKey } from "./LocationInfoGrid"
 import { getPrintableLocationOfInterestString } from "./LocationObjectHandling"
+import { PRESET_LOCATIONS } from "./PresetLocations"
 
 type TodayLocationSummaryProps = {
     locationGroups: any
+}
+
+const getQuickLinkURL = (cityString:string) => {
+    let quickLink = PRESET_LOCATIONS.filter((pl) => pl.urlParam == cityString.toLowerCase())[0];
+    if(quickLink){
+        return `${getHardCodedUrl()}/?loc=${quickLink.urlParam}`
+    }else{
+        return ''
+    }
 }
 
 const TodayLocationSummary = ({locationGroups}:TodayLocationSummaryProps) => {
@@ -15,7 +26,7 @@ const TodayLocationSummary = ({locationGroups}:TodayLocationSummaryProps) => {
         copyText = Object.keys(locationGroups)
             .map(processGroupKey)
             .filter((keyObj:any) => startOfDay(keyObj.date) === startOfDay(new Date()))
-            .map((keyObj:any) => `${keyObj.loc} ${locationGroups[keyObj.key].map(getPrintableLocationOfInterestString).join('')}\n\n`)
+            .map((keyObj:any) => `${keyObj.loc} ${locationGroups[keyObj.key].length > 1 ? `- ${locationGroups[keyObj.key].length} New Locations`: ''}\n${locationGroups[keyObj.key].map(getPrintableLocationOfInterestString).join('')}\n${getQuickLinkURL(keyObj.loc)}\n\n`)
             .join('');
     }
     
