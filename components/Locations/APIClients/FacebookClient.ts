8e78a79cb314bdeb7fe3e6d0fcbb6c701f1448ab
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-01-23 11:32:51 +1300

    Added facebook init

--- components/Locations/APIClients/FacebookClient.ts
+++ components/Locations/APIClients/FacebookClient.ts
@@ -1,4 +1,3 @@
-import FB, {FacebookApiException} from 'fb';
 import { reject, runInContext } from 'lodash';
 import SocialPostRun from './SocialPostRun';
 import SocialPostRunResult from './SocialPostRunResult';
@@ -6,27 +5,29 @@ import SocialPostRunResult from './SocialPostRunResult';
 
 // TODO: add cache invalidation
 class FacebookClient { 
-    overrideDbId!:string;
-    presetDbId!:string;
-
-    cachedLocationOverrides!:LocationOverride[];
-    cachedLocationOverridesUpdateTime!:Date;
-    cachedLocationPresets!:LocationPreset[];
-    cachedLocationPresetsUpdateTime!:Date;
-
-    
+    FB:any
+       
 
     constructor (){
 
         if(!process.env.FB_ACCESS_TOKEN){ console.error('FB_ACCESS_TOKEN not set'); throw 'Config error 01' }
+        if(!process.env.FB_APP_ID){ console.error('FB_APP_ID not set'); throw 'Config error 01' }
+        
 
-        FB.setAccessToken(process.env.FB_ACCESS_TOKEN);
+
+        this.FB = require('fb');
+        FB.init({
+            appId: process.env.FB_APP_ID,
+            version: 'v2.7',
+            status: true
+        })
+
     }
 
     updateFacebook(run:SocialPostRun, title:string, text:string):Promise<SocialPostRun>{
         return new Promise((reject,resolve) => {
             try{
-                FB.api('me/feed', 'post', { message: text }, async function (res:any) {
+                FB.api('me/feed', 'post', { message: text, access_token: process.env.FB_ACCESS_TOKEN }, async function (res:any) {
                 if(!res || res.error) {
                     run.setError(res.error);
                     reject(run);
