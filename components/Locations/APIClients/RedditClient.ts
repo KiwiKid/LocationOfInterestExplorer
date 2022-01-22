9732255f8b335db7cfff8f54df00202508fefc33
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-01-22 15:04:45 +1300

    Make the reddit API calls more resilient

--- components/Locations/APIClients/RedditClient.ts
+++ components/Locations/APIClients/RedditClient.ts
@@ -31,7 +31,7 @@ class RedditClient {
             
         });
         //TODO: can this be 1?
-        r.config({requestDelay: 1500, warnings: false});
+        r.config({requestDelay: 1500, warnings: false, continueAfterRatelimitError: true});
 
         if(!r){ console.error('failed to generate reddit client'); throw 'err'}
 
