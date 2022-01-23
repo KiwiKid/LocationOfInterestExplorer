6e5af66938782ccc751120b502435071753cda19
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-01-23 14:24:28 +1300

    Added facebook client

--- components/Locations/APIClients/FacebookClient.ts
+++ components/Locations/APIClients/FacebookClient.ts
@@ -1,44 +1,77 @@
 import { reject, runInContext } from 'lodash';
 import SocialPostRun from './SocialPostRun';
 import SocialPostRunResult from './SocialPostRunResult';
-// @ts-ignore
-import FB from 'fb'
+import { FacebookApp } from 'fbsdk-ts';
+import { startOfDayNZ, todayNZ } from '../DateHandling';
+import { Touchscreen } from 'puppeteer-core';
+import axios from 'axios';
+
+
 
 // TODO: add cache invalidation
 class FacebookClient { 
-       
+    private app:FacebookApp
+    private appId:string
+    private appSecret:string
+    private appAccessToken:string
+    private facebookPageID
 
     constructor (){
 
         if(!process.env.FB_ACCESS_TOKEN){ console.error('FB_ACCESS_TOKEN not set'); throw 'Config error 01' }
-        if(!process.env.NEXT_PUBLIC_FACEBOOK_APP_ID){ console.error('NEXT_PUBLIC_FACEBOOK_APP_ID not set'); throw 'Config error 01' }
+        if(!process.env.NEXT_PUBLIC_FACEBOOK_APP_ID){ console.error('NEXT_PUBLIC_FACEBOOK_APP_ID not set'); throw 'Config error 02' }
+        if(!process.env.FB_APP_SECRET){ console.error('FB_APP_SECRET not set'); throw 'Config error 03' }
+        if(!process.env.FB_PAGE_ID){ console.error('FB_PAGE_ID not set'); throw 'Config error 04' }
+
         
+        this.appId = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID
+        this.appSecret = process.env.FB_ACCESS_TOKEN    
+        this.appAccessToken = process.env.FB_ACCESS_TOKEN;
+        this.facebookPageID = process.env.FB_PAGE_ID
 
-
-
+        this.app = new FacebookApp({
+            appId: this.appId,
+            appSecret: this.appSecret,
+        });
 
     }
 
-    updateFacebook(run:SocialPostRun, title:string, text:string):Promise<SocialPostRun>{
-        return new Promise((reject,resolve) => {
-            try{
-                FB.init({
-                    appId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID,
-                    version: 'v2.7',
-                    status: true
-                })
+    getFacebookEditUrl(postId:string,){
+        return `https://graph.facebook.com/v3.2/${this.facebookPageID}_${postId}?&access_token=${this.appAccessToken}`
+    }
 
-                FB.api('me/feed', 'post', { message: text, access_token: process.env.FB_ACCESS_TOKEN }, async function (res:any) {
-                if(!res || res.error) {
-                    run.setError(res.error);
-                    reject(run);
-                    return;
+    updateFacebook(run:SocialPostRun, title:string, text:string):Promise<SocialPostRun> {
+        return new Promise(async (resolve, reject) => {
+            try{                
+                const isUpdate = run.lastCheckTime && startOfDayNZ(new Date(run.lastCheckTime)) === startOfDayNZ(todayNZ())
+                if(isUpdate && run.existingPostId) {
+
+
+                   
+                    //this.app.Nodes.PagePost('pageid_'+run.existingPostId).read(
+                    const res = await axios.post(this.getFacebookEditUrl(run.existingPostId), { message: text})
+                                        .then((rr) => processFacebookSubmission(true, true, false, rr.data.id, title));
+
+                    run.setResults(res)
+
+                    resolve(run)
+
+                        //const res = this.app.Nodes.Page('pageid').Edges.Posts (this.appAccessToken, {  app_id: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID, message: text})
+                }else{
+                    //@ts-ignore
+                    const res = this.app.Nodes.Page('pageid').Edges.Posts.create(this.appAccessToken, {  app_id: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID, message: text})
+/*
+                    FB.api('me/feed', 'post', { message: text, access_token: process.env.FB_ACCESS_TOKEN }, async function (res:any) {
+                    if(!res || res.error) {
+                        run.setError(res.error);
+                        reject(run);
+                        return;
+                    }
+                    console.log('Post Id: ' + res.id);*/
+                    //@ts-ignore
+                    run.setResults(await processFacebookSubmission(true, false, false, res.id, title));
+                    resolve(run)
                 }
-                console.log('Post Id: ' + res.id);
-                
-                run.setResults(await processFacebookSubmission(true, true, false, run, res.id, title))
-                resolve(run)
-                });
             }catch(err){
                 run.setError(err);
                 reject(run);
@@ -48,7 +81,7 @@ class FacebookClient {
 }
 
 
-const processFacebookSubmission = async (isSuccess:boolean, isUpdate:boolean, isSkipped: boolean, run:SocialPostRun, subId:string, title:string):Promise<SocialPostRunResult> => { 
+const processFacebookSubmission = async (isSuccess:boolean, isUpdate:boolean, isSkipped: boolean, subId:string, title:string):Promise<SocialPostRunResult> => { 
     return new SocialPostRunResult(isSuccess, isUpdate, isSkipped, title, subId);
 }
 
