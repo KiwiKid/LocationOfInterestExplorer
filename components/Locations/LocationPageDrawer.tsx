3a4018996ee97ce93b769d0b50eac184cd3209e2
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-02-02 09:20:37 +1300

    Improve FAQ

--- components/Locations/LocationPageDrawer.tsx
+++ components/Locations/LocationPageDrawer.tsx
@@ -389,8 +389,8 @@ const LocationPageDrawer = ({
                     <p>(I did notice the map performed better when running the site as an app!)</p>
                   </Question>
                   <Question title="What are you doing with my location!?!">
-                      <p>Your location is only used to position the map.<br/>It is not stored.</p>
-                      <p>If you would like, you can use the link in the {`"Share"`} section to restore the same view you left off with.</p>
+                      Your location is only used to position the map.<br/>It is not stored.<br/>
+                      If you would like, you can use the link in the {`"Share"`} section to restore the same view you left off with.
                     </Question>     
                     {process.env.NEXT_PUBLIC_FEEDBACK_URL && <Question   title="X is does not work, is broken, or unclear?">
                       <div className="md:w-3/5 m-auto">
@@ -408,8 +408,6 @@ const LocationPageDrawer = ({
                         <p>Static locations (i.e. not buses or flights) have a latitude and longitude coordinate pair.</p>
                         <p>These locations are then projected into the map and with a bit of good ol&apos; fashion trigonometry we can determine if the location is in the circle and display the detailed information for it.</p>
                               
-                        <p>Flights are mapped at both the destination and origin airports</p>
-                        
                         <p>The raw data this service is based on:
                           <div className="pt-2 max-w-3xl">
                             <ExternalLink
@@ -417,9 +415,6 @@ const LocationPageDrawer = ({
                               href="https://github.com/minhealthnz/nz-covid-data/tree/main/locations-of-interest"
                               />
                           </div>
-                        </p>
-                        <p>Some locations are overridden manually where they are obviously incorrect.  
-                          <link href={publishState.hardcodedURL} className="underline" rel="noreferrer" >Raw list of overrides</link>
                         </p>
                     </Question>
                     <Question title="Why are you doing this?">
