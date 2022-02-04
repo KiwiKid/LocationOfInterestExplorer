9f11ff58a031dbeb28982d60a609a42a39b3eae9
Greg Cumming <KiwiKid@users.noreply.github.com>
2022-02-04 13:30:39 +1300

    Improve loading screen display

--- components/Locations/Loading.tsx
+++ components/Locations/Loading.tsx
@@ -7,7 +7,7 @@ type LoadingProps = {
 const Loading = ({locationTitle}:LoadingProps) => {
 
     return (
-        <div style={{ width: '100%' }}>
+        <div style={{ width: '100%', textAlign: 'center' }}>
             <div style={{maxWidth: '400px', margin: 'auto'}}><Image width="400px" src="/img/icon.gif" height="400px" alt="loading icon" /></div>
             <div style={{maxWidth: '600px', margin: 'auto'}}>Loading a map of the latest Covid-19 Locations of Interest {locationTitle ? <>for <span style={{ fontWeight: '600'}}>{locationTitle}</span></>: ''} directly from the Ministry of Health...</div>
         </div>
